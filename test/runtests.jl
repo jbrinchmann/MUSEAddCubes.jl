333588eff0456e59d03def9b1ba84471f7d2142f
Jarle Brinchmann <jbrinchmann@gmail.com>
2023-05-08 16:02:19 +0100

    Added tests of combination code

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,9 @@ using MUSEAddCubes
 using Test
 
 @testset "MUSEAddCubes.jl" begin
-    # Write your tests here.
+    m, ma = MUSEAddCubes.median_stack([1.0,2,3])
+    μ, σ = MUSEAddCubes.mean_stack([1.0,3.0,5.0])
+    
+    @test [m, ma] ≈ [2.0, 1.4826022185] atol=1
+    @test [μ, σ] == [3.0, 2.0]
 end

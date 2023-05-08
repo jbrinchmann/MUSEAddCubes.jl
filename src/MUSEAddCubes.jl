cd1e830d517d08af0451d5b85b5fdd0db08a7d92
Jarle Brinchmann <jbrinchmann@gmail.com>
2023-05-08 16:02:06 +0100

    Added inclusion of files

--- src/MUSEAddCubes.jl
+++ src/MUSEAddCubes.jl
@@ -1,5 +1,20 @@
 module MUSEAddCubes
 
-# Write your package code here.
+#
+# This is a package to be able to add many aligned MUSE cubes
+# together. The relevant situation here is when there are many cubes
+# and they do not fit in memory at the same time.
+#
+# Thus this does the combination wavelength-layer by wavelength-layer
+#
+# The
+#
+
+
+include("io.jl")
+include("combine.jl")
+include("driver.jl")
+
+
 
 end

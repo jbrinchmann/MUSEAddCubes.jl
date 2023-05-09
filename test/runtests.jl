9db83b103c276b40f26b358d5310ad5e2331b0e6
Jarle Brinchmann <jbrinchmann@gmail.com>
2023-05-09 12:08:03 +0100

    Added test for when NaN is present

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,4 +7,10 @@ using Test
     
     @test [m, ma] ≈ [2.0, 1.4826022185] atol=1
     @test [μ, σ] == [3.0, 2.0]
+
+    # Test the impact of NaNs
+    m, ma = MUSEAddCubes.median_stack([1.0,2,3, NaN, 10.0, 1.0])
+
+    @test m == 2.0
+
 end

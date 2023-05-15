3cf251a7920d4385a4fbebb9f07b468a24df9414
Jarle Brinchmann <jbrinchmann@gmail.com>
2023-05-15 23:34:56 +0100

    Added a couple of tests with non-Float input

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test
     m, ma, N1 = MUSEAddCubes.median_stack([1.0,2,3])
     μ, σ, N2 = MUSEAddCubes.mean_stack([1.0,3.0,5.0])
     
-    @test [m, ma] ≈ [2.0, 1.4826022185] atol=1
+    @test [m, ma] ≈ [2.0, 1.4826022185] atol=1e-4
     @test [μ, σ] == [3.0, 2.0]
     @test N1 == 3
     
@@ -18,7 +18,19 @@ using Test
     @test isnan(m)
     @test N == 0
 
+    
+    #
+    # Test different types of input
+    #
+    m, ma, N1 = MUSEAddCubes.median_stack([1,2,3])
+    μ, σ, N2 = MUSEAddCubes.mean_stack([1//2,3//4,5//5])
 
+    # I would have preferred median to return the same type as the input array
+    # but it doesn't so let's stay
+    @test [m, ma] ≈ [2.0, 1.4826022185] atol=1e-4
+    @test [μ, σ] == [3//4, 0.25]
+
+    
     #
     # Test the flattening of the cube
     # Since median and mean have both been tested separately above,
@@ -34,7 +46,7 @@ using Test
     m, MAD = MUSEAddCubes.flatten_stack(c, "median")
     @test m[2,2] == 1.5
     @test MAD[2,2] ≈ 0.7413011092528009 atol=1e-3
-    
+
     
     
 end
